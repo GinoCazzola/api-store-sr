ef3c60c5211d3086b2fe5bf05f63c9ffed2805e8
Gino Cazzola <ginex.bluex@gmail.com>
2024-06-04 14:08:54 -0300

    Save

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import productsRoutes from "./src/routes/products.routes.js";
-import { dbConection } from "./src/data/dbConection.js";
+import imagesRoutes from "./src/routes/images.routes.js";
+import messagesRoutes from "./src/routes/messages.routes.js";
+import cartRoutes from "./src/routes/cart.routes.js";
+import checkoutRoutes from "./src/routes/checkout.routes.js";
 
+import { dbConection } from "./src/data/dbConection.js";
 
 const server = express()
 
@@ -12,13 +16,18 @@ dotenv.config()
 
 const api = async () => {
     await dbConection()
+
     server.use(express.json())
     server.use(cors())
-    server.use('/public', express.static( `./temp/imgs`))
+    
+    server.use('/public', express.static(`./temp/imgs`))
+    server.use("/images", imagesRoutes)
     server.use("/api/products", productsRoutes)
-
-server.listen(process.env.PORT, () => console.log ( `servidor corriento en el puerto${process.env.PORT}` ))
-
+    server.use("/api/messages", messagesRoutes)
+    server.use("/api/cart", cartRoutes)
+    server.use("/api/checkout", checkoutRoutes)
+    
+    server.listen(process.env.PORT, () => console.log(`Servidor corriendo en el puerto ${process.env.PORT}`))
 }
 
 api()
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { createProduct, getProducts } from "../controllers/prodController.js";
+
+import { createProduct, deleteProduct, editProduct, getProducts } from "../controllers/prodController.js";
+
 import upload from "../libs/main.storage.js";
+
 
 const route = express.Router()
 
 route
-.post("/", upload.single("image"), createProduct)
-.get("/", getProducts)
-
+    .post("/", upload.single("image"), createProduct)
+    .get("/", getProducts)
+    .put("/edit/:id", upload.single("image"), editProduct)
+    .delete("/delete/:id", deleteProduct)
 
 export default route;
