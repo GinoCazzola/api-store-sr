65e92d5b4792bedf423f0e3b8498d180fac30f26
Gino Cazzola <ginex.bluex@gmail.com>
2024-06-05 15:24:29 -0300

    Important updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import productsRoutes from "./src/routes/products.routes.js";
 import imagesRoutes from "./src/routes/images.routes.js";
 import messagesRoutes from "./src/routes/messages.routes.js";
 import cartRoutes from "./src/routes/cart.routes.js";
-import checkoutRoutes from "./src/routes/checkout.routes.js";
+
 
 import { dbConection } from "./src/data/dbConection.js";
 
@@ -25,7 +25,7 @@ const api = async () => {
     server.use("/api/products", productsRoutes)
     server.use("/api/messages", messagesRoutes)
     server.use("/api/cart", cartRoutes)
-    server.use("/api/checkout", checkoutRoutes)
+    
     
     server.listen(process.env.PORT, () => console.log(`Servidor corriendo en el puerto ${process.env.PORT}`))
 }
--- src/routes/checkout.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createCheckoutPreference } from '../controllers/CheckoutController.js';
-
-const route = express.Router();
-
-route
-    .post("/", createCheckoutPreference)
-
-export default route;
