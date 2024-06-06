1cba0fc7c0d59f94d4f812bd4e24b4442848371f
Gino Cazzola <ginex.bluex@gmail.com>
2024-06-06 18:26:55 -0300

    Cambios importantes 2

--- index.js
+++ index.js
@@ -7,32 +7,27 @@ import imagesRoutes from "./src/routes/images.routes.js";
 import messagesRoutes from "./src/routes/messages.routes.js";
 import cartRoutes from "./src/routes/cart.routes.js";
 
-import { dbConection } from "./src/data/dbConection.js";
 
-dotenv.config();
+import { dbConection } from "./src//data/dbConection.js";
 
-const server = express();
+const server = express()
 
-const allowedOrigins =
-['https://eit-sr-git-master-ginos-projects-67846505.vercel.app',
- 'http://localhost:3000'];
+dotenv.config()
 
 const api = async () => {
-    await dbConection();
+    await dbConection()
 
-    server.use(express.json());
-    server.use(cors({
-        origin: allowedOrigins,
-        credentials: true // Esto es necesario si estás manejando cookies o sesiones
-    }));
+    server.use(express.json())
+    server.use(cors())
     
-    server.use('/public', express.static(`./temp/imgs`));
-    server.use("/images", imagesRoutes);
-    server.use("/api/products", productsRoutes);
-    server.use("/api/messages", messagesRoutes);
-    server.use("/api/cart", cartRoutes);
+    server.use('/public', express.static(`./temp/imgs`))
+    server.use("/images", imagesRoutes)
+    server.use("/api/products", productsRoutes)
+    server.use("/api/messages", messagesRoutes)
+    server.use("/api/cart", cartRoutes)
     
-    server.listen(process.env.PORT, () => console.log(`Servidor corriendo en el puerto ${process.env.PORT}`));
+    
+    server.listen(process.env.PORT, () => console.log(`Servidor corriendo en el puerto ${process.env.PORT}`))
 }
 
-api();
+api()
